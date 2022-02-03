b3d552b6ddc3288a4e302c9e2cf207f7ab9f4295
SeungWookHan <hswook12@me.com>
2022-02-03 12:30:15 +0900

    Add decorator class

--- chapter5/46_decorator_parameter.py
+++ chapter5/46_decorator_parameter.py
@@ -10,7 +10,8 @@
 """
 크게 보면 데코레이터는 함수를 파라미터로 받아서 함수를 반환하는 함수로써 고차 함수(higher-order-function)이라고 할 수 있다.
 """
-from black import E
+from ast import With
+from functools import wraps
 
 
 class ControlledException(Exception):
@@ -54,3 +55,36 @@ def run_with_custom_exception(task):
 def run_with_custom_parameters(task):
   return task.run()
 
+# 데코레이터 객체
+class WithRetry:
+  def __init__(self, retries_limit=RETRIES_LIMIT, allowed_exceptions=None):
+    self.retries_limit = retries_limit
+    self.allowed_exception = allowed_exceptions
+  
+  def __call__(self, operation):
+    
+    @wraps(operation)
+    def wrapped(*args, **kwagrs):
+      last_raised = None
+      
+      for _ in range(self.retries_limit):
+        try:
+          return operation(*args, **kwargs)
+        except self.allowed_exception as e:
+          logger.info("retrying %s due to %s", operation, e)
+          last_raised = e
+      raise last_raised
+    return wrapped
+
+# 위 데코레이터를 함수에 적용
+@WithRetry(retries_limit=5)
+def run_with_custom_retries_limit2(task):
+  return task.run()
+
+  """
+  1. 먼저 @ 연산 전에 전달된 파라미터를 사용해 데코레이터 객체 생성
+  2. __init__ 메서드에서 정해진 로직에 따라 초기화
+  3. @ 연산 호출
+  4. with_custom_retries_limit2 함수를 래핑하여 __call__ 매직 메서드 호출
+  5. 원본 함수를 래핑하여 우리가 원하는 로직이 적용된 새로운 함수 반환
+  """
