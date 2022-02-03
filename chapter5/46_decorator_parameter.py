df8625347b67f88fd2bdb04497e2a2ce4957f4ee
SeungWookHan <hswook12@me.com>
2022-02-03 12:10:11 +0900

    Add indirection decorator

---
+++ chapter5/46_decorator_parameter.py
@@ -0,0 +1,56 @@
+"""
+데코레이터의 강력함은 파라미터를 전달 받아 로직을 추상화한다면 더더욱 강력해진다.
+1. 간접 참조(indirection)를 통해 새로운 레벨의 중첩 함수 만들기
+2. 데코레이터를 위한 클래스 만들기
+
+일반적으로 2번이 가독성이 좋은데, 중첩된 클로저 함수보다는 객체가 이해하기 쉽기 때문.
+"""
+
+# 중첩 함수의 데코레이터
+"""
+크게 보면 데코레이터는 함수를 파라미터로 받아서 함수를 반환하는 함수로써 고차 함수(higher-order-function)이라고 할 수 있다.
+"""
+from black import E
+
+
+class ControlledException(Exception):
+  """도메인에서 발생하는 일반적인 예외"""
+  
+RETRIES_LIMIT = 3
+
+def with_retry(retries_limit=RETRIES_LIMIT, allowed_exceptions=None):
+  allowed_exceptions = allowed_exceptions or (ControlledException,)
+
+  def retry(operation):
+    @wraps(operation)
+    def wrapped(*args, **kwars):
+      last_raised = None
+      for _ in range(retries_limit):
+        try:
+          return operation(*args, **kwargs)
+        except allowed_exceptions as e:
+          logger.info("retrying %s due to %s", operation, e)
+          last_raised = e
+      raise last_raised
+    return wrapped
+  return retry
+
+# 위 데코레이터를 함수에 적용
+@with_retry()
+def run_operation(task):
+  return task.run()
+
+@with_retry(retries_limit=5)
+def run_with_custom_retries_limit(task):
+  return task.run()
+
+@with_retry(allowed_exceptions=(AttributeError,))
+def run_with_custom_exception(task):
+  return task.run()
+
+@with_retry(
+  retries_limit=4, allowed_exceptions=(ZeroDivisionError, AttributeError)
+)
+def run_with_custom_parameters(task):
+  return task.run()
+
